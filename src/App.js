8d1a71e8254adad44be0ab654a09f2e7fa6d9039
a-m-shelare <141905935+a-m-shelare@users.noreply.github.com>
2023-10-06 08:55:31 +0530

    Add files via upload
    creating react application using npx and npm using comand line and with the help of create-react-app

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <article>
+        second way to create react application first check if node is install or
+        not for that use command promnt or anyouther terminal like windows
+        powershell node -v navicate into directory cd documents cd tuts npm
+        create-react-app akash-blogs then enter cd akash-blogs code .
+        <p>
+          <h1>akash shelare </h1>
+        </p>
+      </article>
+    </div>
+  );
+}
+
+export default App;
